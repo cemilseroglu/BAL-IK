e366271b0b8af352af2ec503bba259d55bf71d14
ermanucer <ermanucer@gmail.com>
2022-05-10 17:05:34 +0300

    Mappingler eklendi.

--- BAL-IK.Model/Entities/Personeller.cs
+++ BAL-IK.Model/Entities/Personeller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,18 @@ namespace BAL_IK.Model.Entities
     
     public class Personeller:BasePerson
     {
+        [Required,Key]
         public int PersonelId { get; set; }
+        [Required]
         public int YillikIzinHakki { get; set; } //WHAT CAN I DO SOMETİMES?LOOK AT THE TABELA!
         public DateTime IseBaslama { get; set; }
         public DateTime IstenAyrilma { get; set; }
+        [Required]
         public int SirketID { get; set; }
         public Sirket Sirket { get; set; }
+        [Required]
         public int DepartmanID { get; set; }
         public Departmanlar Departman { get; set; }
-
-        
-
         public List<Zimmetler> Zimmetler { get; set; }
         public List<Izinler> Izinler { get; set; }
         public List<OzlukBelgesi> OzlukBelgeleri { get; set; } 
