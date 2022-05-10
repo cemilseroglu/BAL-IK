f7ce7a85ee6522e9caf190b1848736e0e46eef17
Cemalettin Yılmaz <cemalettin0yilmaz@gmail.com>
2022-05-10 16:33:48 +0300

    SirketAdi required yapildi.

--- BAL-IK.Model/Entities/Sirket.cs
+++ BAL-IK.Model/Entities/Sirket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BAL_IK.Model.Entities
     public class Sirket
     {
         public int SirketId { get; set; }
+        [Required]
         public string SirketAdi { get; set; }
         public string SirketAdresi { get; set; }
         public string SirketTelefonu { get; set; }
