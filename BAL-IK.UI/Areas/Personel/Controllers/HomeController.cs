8feba7a56b1d9674631e303857463e90a711a160
Cemalettin Yılmaz <cemalettin0yilmaz@gmail.com>
2022-05-11 16:21:57 +0300

    PersonellerHomeController Eklendi

---
+++ BAL-IK.UI/Areas/Personel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BAL_IK.UI.Areas.Personel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
