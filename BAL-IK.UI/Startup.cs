b326abb1bade9291a5ed5ba362180c6929c35725
Cemalettin Yılmaz <cemalettin0yilmaz@gmail.com>
2022-05-11 15:47:37 +0300

    Arealar eklendi.

--- BAL-IK.UI/Startup.cs
+++ BAL-IK.UI/Startup.cs
@@ -45,6 +45,10 @@ namespace BAL_IK.UI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
