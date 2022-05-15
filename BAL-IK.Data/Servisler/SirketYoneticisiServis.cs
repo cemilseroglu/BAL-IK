a6c443b1ddd8d56312e30281c6367258d5aaafc4
ermanucer <ermanucer@gmail.com>
2022-05-15 19:07:21 +0300

    SirketYoneticisi islemleri yapildi.

---
+++ BAL-IK.Data/Servisler/SirketYoneticisiServis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL_IK.Data.Servisler
+{
+    public class SirketYoneticisiServis
+    {
+    }
+}
---
+++ BAL-IK.Model/RequestClass/SirketYoneticisiIslemleriRequest.cs
@@ -0,0 +1,15 @@
+﻿using BAL_IK.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL_IK.Model.RequestClass
+{
+    public class SirketYoneticisiIslemleriRequest
+    {
+       
+    }
+}
