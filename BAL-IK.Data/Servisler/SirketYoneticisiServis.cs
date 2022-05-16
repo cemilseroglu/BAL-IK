b486b47bc977848d6b4880fc4d079701389ac4c6
ermanucer <ermanucer@gmail.com>
2022-05-16 17:02:53 +0300

    template eklendi.

--- BAL-IK.Data/Servisler/SirketYoneticisiServis.cs
+++ BAL-IK.Data/Servisler/SirketYoneticisiServis.cs
@@ -19,7 +19,7 @@ namespace BAL_IK.Data.Servisler
         public SirketYoneticisiServis(BAL_IKContext db)
         {
             _db = db;
-        }    
+        }
 
         public SirketYoneticisiGuncel SirketYoneticisiGuncelle(SirketYoneticisiIslemleriRequest.SirketYoneticisiGuncelle sirketYoneticisi)
         {
@@ -28,16 +28,25 @@ namespace BAL_IK.Data.Servisler
             try
             {
                 SirketYoneticisi syoneticisi = _db.SirketYoneticileri.Find(sirketYoneticisi.SirketYoneticisiId);
-                if(sirketYoneticisi.Ad!=null)
-                    syoneticisi.Ad = sirketYoneticisi.Ad;                
-                syoneticisi.Soyad = sirketYoneticisi.Soyad;
+                if (sirketYoneticisi.Ad != null)
+                    syoneticisi.Ad = sirketYoneticisi.Ad;
+                if (sirketYoneticisi.Soyad != null)
+                    syoneticisi.Soyad = sirketYoneticisi.Soyad;
+                if (sirketYoneticisi.Eposta != null)
+                    syoneticisi.Eposta = sirketYoneticisi.Eposta;
+                if (sirketYoneticisi.Sifre != null)
+                    syoneticisi.Sifre = sirketYoneticisi.Sifre;
                 syoneticisi.DogumTarihi = sirketYoneticisi.DogumTarihi;
-                syoneticisi.Eposta = sirketYoneticisi.Eposta;
-                syoneticisi.Sifre = sirketYoneticisi.Sifre;
-               
+
+
                 _db.Update(syoneticisi);
                 _db.SaveChanges();
                 resp.SirketYoneticisiId = syoneticisi.SirketYoneticisiId;
+                resp.Ad = syoneticisi.Ad;
+                resp.Soyad = syoneticisi.Soyad;
+                resp.DogumTarihi = syoneticisi.DogumTarihi;
+                resp.Eposta = syoneticisi.Eposta;
+                resp.Sifre = syoneticisi.Sifre;
                 resp.BasariliMi = true;
                 resp.Mesaj = "Başarıyla güncellendi.";
                 return resp;
@@ -49,7 +58,7 @@ namespace BAL_IK.Data.Servisler
                 resp.Mesaj = ex.Message;
                 return resp;
             }
-        
+
         }
     }
 }
