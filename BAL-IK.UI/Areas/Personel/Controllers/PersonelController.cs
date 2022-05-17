3562e63201550be4533420b85495e208a125440f
ermanucer <ermanucer@gmail.com>
2022-05-17 15:26:20 +0300

    Sirket yoneticisi eklemeler yapildi.

--- BAL-IK.UI/Areas/Personel/Controllers/PersonelController.cs
+++ BAL-IK.UI/Areas/Personel/Controllers/PersonelController.cs
@@ -5,12 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace BAL_IK.UI.Areas.Personel.Controllers
 {   
     [Area("Personel")]
-    [Personel]
+    //[Personel]
     public class PersonelController : Controller
     {
         public IActionResult Index()
         {
-           var personelGuid= HttpContext.Session.GetString("personel");
+           //var personelGuid= HttpContext.Session.GetString("personel");
          
             return View();
         }
