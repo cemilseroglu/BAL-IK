5e7cd30ea8fca23722093d970061eee3b36f3eef
Berna Dereağzı <bernaarmarosss@gmail.com>
2022-05-17 17:02:28 +0300

    Ayarlar controller

--- BAL-IK.UI/Areas/Personel/Controllers/PersonelController.cs
+++ BAL-IK.UI/Areas/Personel/Controllers/PersonelController.cs
@@ -2,11 +2,12 @@
 using BAL_IK.UI.Filters;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static BAL_IK.Model.RequestClass.PersonelIslemleriRequest;
 
 namespace BAL_IK.UI.Areas.Personel.Controllers
 {   
     [Area("Personel")]
-    //[Personel]
+   // [Personel]
     public class PersonelController : Controller
     {
 
@@ -16,23 +17,28 @@ namespace BAL_IK.UI.Areas.Personel.Controllers
         {
             this._personelService = personelService;
         }
+      [HttpPost]
         public IActionResult Index(string guid)
         {
+            
             var response = _personelService.PersonelGetir(guid);
+           
             if (response == null)
                 return BadRequest();
             if (response.BasariliMi == false)
             {
+                var personelGuid = HttpContext.Session.GetString("personel");
                 ViewBag.Mesaj = response.Mesaj;
                 return View(guid);
             }
             return View();
         }
+    
         public IActionResult Index()
         {
-           var personelGuid= HttpContext.Session.GetString("personel");
+           
          
-            return View();
+           return View();
         }
 
         public IActionResult Ayarlar()
@@ -41,6 +47,13 @@ namespace BAL_IK.UI.Areas.Personel.Controllers
         }
         public IActionResult Duzenle()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Duzenle(PersonelGuncelle pr)
+        {
+            var response = _personelService.PersonelGuncelleme(pr);
+            ViewBag.Mesaj = response.Mesaj;
             return View();
         }
 
